92c461c4e469c4106bf40882246fe7fa3270264f
mueez <mueezejaz12345678@gmail.com>
2025-01-24 21:00:17 +0500

    made some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,79 +6,96 @@ import { FaFacebook } from "react-icons/fa6";
 import toprightmg from "../assets/topleftmg.webp"
 import bottomrightmg from "../assets/bottomrightmg.webp"
 import simpleanimebg from "../assets/singleanimbc.webp"
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FiTwitter } from "react-icons/fi";
 import wave from "../assets/wave.svg"
+import { RxHamburgerMenu } from "react-icons/rx";
 import { motion } from 'motion/react';
 const Home = () => {
   const [loopAnimation, setLoopAnimation] = useState(false);
-  let text = "We Have The Ability To Stay Connect With You"
+  const Htext1 = "CONNECT YOURSELF"
+  const Htext2 = "WITH US"
+  const ptext1 = "We Have The Ability To Stay"
+  const ptext2 = "Connect With You"
+
   return (
     <>
       <nav className='nav'>
         <img src={logopng} alt="" />
-        {/* <ul>
+        <div className='hamcontainer'>< RxHamburgerMenu className='hamburger' size={30} /></div>
+        <ul>
       <li>Home</li>
       <li>About</li>
       <li>Services</li>
       <li>Portfolio</li>
       <li>contact us</li>
-    </ul> */}
+    </ul>
       </nav>
       <div className='page1'>
          
         <div className='contactinfo'>
           <div className='gmail'><MdEmail size={20} color='white' /><p>hubitservices@gmail.com</p></div>
-          <div className='othercontact'><FaFacebook color='white' size={25} /><FaFacebook color='white' size={25} /><FaFacebook color='white' size={25} /></div>
+          <div className='othercontact'><FaFacebook color='white' size={25} /><AiOutlineInstagram color='white' size={25} /><FiTwitter color='white' size={25} /></div>
         </div>
         <div className="line"></div>
         <div className="maincontaint">
-          <h1>
-            <span><motion.img
-              initial={{ right: "-100%" }}
-              animate={{ right: "120%" }}
-              transition={{ ease: "easeInOut", duration: 1 }}
-              src={simpleanimebg} alt="" className='mainheadingmagnet' /></span>
-            <motion.span
-              initial={{ right: "-100%" }}
-              animate={{ right: "0" }}
-              transition={{ ease: "easeInOut", duration: 0.6, delay: 0.4 }}
-              className='mainheading'>CONNECT
-              YOURSELF</motion.span>
-              </h1>
-               <h1>
-              <motion.span
-              initial={{ left: "-100%" }}
-              animate={{ left: "0" }}
-              transition={{ ease: "easeInOut", duration: 0.6, delay: 0.4 }}
-              className='mainheading2'>WITH US
-              </motion.span>
-              <span>
-              <motion.img
-              initial={{ left: "-100%" }}
-              animate={{ left: "120%" }}
-              transition={{ ease: "easeInOut", duration: 1 }}
-              src={simpleanimebg} alt="" className='mainheadingmagnet2' /></span>
-                
-                </h1>
-          <p><motion.span
-          initial={{ opacity: "0" }}
-          animate={{ opacity: "1" }}
-          transition={{ ease: "easeInOut", duration: 0.5 ,delay:1}}
-          >{"{"}</motion.span><motion.div
-          initial={{ width: "0" }}
-          animate={{ width: "100%" }}
-          transition={{ ease: "easeInOut", duration: 0.5, delay: 1 }}
-          >{text}</motion.div><motion.span
-          initial={{ opacity: "0" }}
-          animate={{ opacity: "1" }}
-          transition={{ ease: "easeInOut", duration: 0.5, delay:1}}
-          >{"}"}</motion.span></p>
+       <h1>
+       {Htext1.split('').map((char, i) => (
+          <motion.span
+            key={i}
+            initial={{ opacity: 0, left: "-10px" }}
+            animate={{ opacity: 1,  left: "0px"}}
+            exit="hidden"
+            transition={{ duration: 0.5, delay: i * 0.1 }}
+          >
+            {char === ' ' ? <span>&nbsp;</span> : char}
+          </motion.span>
+        ))}
+         <br />
+         {Htext2.split('').map((char, i) => (
+          <motion.span
+            key={i}
+            initial={{ opacity: 0, left: "-10px" }}
+            animate={{ opacity: 1,  left: "0px"}}
+            exit="hidden"
+            transition={{ duration: 0.5, delay: Htext1.length * 0.1 + i * 0.1, }}
+          >
+            {char === ' ' ? <span>&nbsp;</span> : char}
+          </motion.span>
+        ))}
+       </h1>
+       <p>
+       {ptext1.split('').map((char, i) => (
+          <motion.span
+            key={i}
+            initial={{ opacity: 0, left: "-10px" }}
+            animate={{ opacity: 1,  left: "0px"}}
+            exit="hidden"
+            transition={{ duration: 0.3, delay:  Htext1.length * 0.1+ptext2.length * 0.1 + i * 0.03, }}
+          >
+            {char === ' ' ? <span>&nbsp;</span> : char}
+          </motion.span>
+        ))}
+        <br />
+        {ptext2.split('').map((char, i) => (
+          <motion.span
+            key={i}
+            initial={{ opacity: 0, left: "-10px" }}
+            animate={{ opacity: 1,  left: "0px"}}
+            exit="hidden"
+            transition={{ duration: 0.3, delay:  Htext1.length * 0.1+ptext2.length * 0.1 + i * 0.03, }}
+          >
+            {char === ' ' ? <span>&nbsp;</span> : char}
+          </motion.span>
+        ))}
+       </p>
           <motion.button
-          initial={{ opacity: 0, }}
-          animate={{ opacity: 1, }}
+          initial={{ opacity: 0,y:5}}
+          animate={{ opacity: 1, y:0}}
           transition={{
-            ease: [0.42, 0, 0.58, 1],
-            duration: 1.5, 
-            delay: 2,
+            ease:"easeIn",
+            duration: 0.3, 
+            delay: 3,
           }}
           >LEARN MORE</motion.button>
         </div>
